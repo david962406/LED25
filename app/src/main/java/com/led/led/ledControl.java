b703852d225b7ced9e781c142cfc55446ef36720
JoseCrisAvalos <cristianavalose@gmail.com>
2017-05-14 18:16:41 -0500

    vgh vghjiokoj

--- app/src/main/java/com/led/led/ledControl.java
+++ app/src/main/java/com/led/led/ledControl.java
@@ -25,7 +25,7 @@ public class ledControl extends ActionBarActivity {
 
     Button btnOn, btnOff, btnDis;
     SeekBar brightness;
-    sgcgbguighubhui
+    sgcgbguighubhuihcfhgvbkvyucjhmvb
     //jhfhvyufhvvhj
     TextView lumn;
     String address = null;
